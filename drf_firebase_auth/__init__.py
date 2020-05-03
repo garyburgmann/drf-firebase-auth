8ecc9c3ccb28f6b8b4f9ce663454d6fe8c23ab77
Gary Burgmann <garyburgmann@gmail.com>
2020-05-04 09:51:59 +1000

    pulled exception updates and tag 0.0.8

--- drf_firebase_auth/__init__.py
+++ drf_firebase_auth/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'drf-firebase-auth'
-__version__ = '0.0.6'
+__version__ = '0.0.8'
 __author__ = 'Gary Burgmann'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2019 Gary Burgmann'
+__copyright__ = 'Copyright 2020 Gary Burgmann'
 
 # Version synonym
 VERSION = __version__
