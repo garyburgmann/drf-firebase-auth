049b40c9db403c20b6693efb5ea1d3807181998b
Gary Burgmann <garyburgmann@gmail.com>
2021-02-04 13:53:24 +1000

    1.0.0 - release ready

--- drf_firebase_auth/__init__.py
+++ drf_firebase_auth/__init__.py
@@ -1,11 +1,11 @@
 """Metadata for drf-firebase-auth package."""
 __title__ = 'drf-firebase-auth'
-__version__ = '1.0.0dev1'
+__version__ = '1.0.0'
 __description__ = (
     'Custom Django Rest Framework authentication backend for '
     'parsing Firebase uid tokens and storing as local users.'
 )
-__url__ = 'https://gitlab.com/garyburgmann/drf-firebase-auth'
+__url__ = 'https://github.com/garyburgmann/drf-firebase-auth'
 __author__ = 'Gary Burgmann'
 __author_email__ = 'garyburgmann@gmail.com'
 __license__ = 'MIT'
