edbfee75e81c7515bf43033f154433034a0de8f2
Ahmane Samir <samirahmane.trading@gmail.com>
2021-07-23 14:54:57 +0100

    feat: removed unnecessary mapping functions for the username field

--- drf_firebase_auth/utils.py
+++ drf_firebase_auth/utils.py
@@ -29,49 +29,10 @@ def get_firebase_user_identifier(firebase_user: auth.UserRecord) -> str:
         raise Exception(e)
 
 
-def map_firebase_to_username_legacy(firebase_user: auth.UserRecord) -> str:
-    try:
-        username = '_'.join(
-            firebase_user.display_name.split(' ')
-            if firebase_user.display_name
-            else str(uuid.uuid4())
-        )
-        return username if len(username) <= 30 else username[:30]
-    except Exception as e:
-        raise Exception(e)
-
-
-def map_firebase_display_name_to_username(
-    firebase_user: auth.UserRecord
-) -> str:
-    try:
-        return '_'.join(firebase_user.display_name.split(' '))
-    except Exception as e:
-        raise Exception(e)
-
-
 def map_firebase_uid_to_username(
     firebase_user: auth.UserRecord
 ) -> str:
     try:
         return firebase_user.uid
-    except Exception as e:
-        raise Exception(e)
-
-
-def map_firebase_email_to_username(
-    firebase_user: auth.UserRecord
-) -> str:
-    try:
-        return get_firebase_user_identifier(firebase_user)
-    except Exception as e:
-        raise Exception(e)
-
-
-def map_uuid_to_username(
-    _: auth.UserRecord
-) -> str:
-    try:
-        return str(uuid.uuid4())
     except Exception as e:
         raise Exception(e)
